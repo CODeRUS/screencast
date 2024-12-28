ec16c24830314f551fa98c59fbcfa96d65988df2
CODeRUS <coderusinbox@gmail.com>
2024-12-28 20:40:25 +0100

    Translations (#8)

---
+++ settings/translations/screencast-ru.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>mainpage</name>
+    <message>
+        <location filename="../mainpage.qml" line="36"/>
+        <source>Screencast</source>
+        <translation>Screencast</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="40"/>
+        <source>Main options</source>
+        <translation>Основные настройки</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="50"/>
+        <source>Buffers to store frames</source>
+        <translation>Буфер для хранения кадров</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="51"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="64"/>
+        <source>JPEG frames quality</source>
+        <translation>Качество jpeg кадров</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="65"/>
+        <location filename="../mainpage.qml" line="79"/>
+        <source>%1%</source>
+        <translation>%1%</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="78"/>
+        <source>Frames scale</source>
+        <translation>Масштаб кадров</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="88"/>
+        <source>Apply smooth transformation</source>
+        <translation>Плавное масштабирование</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="98"/>
+        <source>Flush packages</source>
+        <translation>Ждать отправку пакетов</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="107"/>
+        <source>Authorization</source>
+        <translation>Авторизация</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="113"/>
+        <location filename="../mainpage.qml" line="114"/>
+        <source>Username</source>
+        <translation>Имя пользователя</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="127"/>
+        <location filename="../mainpage.qml" line="128"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="138"/>
+        <source>You can connect to</source>
+        <translation>Адрес для подключения</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="147"/>
+        <location filename="../mainpage.qml" line="148"/>
+        <source>
+http://%1%2:5554</source>
+        <translation>
+http://%1%2:5554</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../mainpage.qml" line="152"/>
+        <source>%n connected clients</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>%n клиент подключен</numerusform>
+            <numerusform>%n клиента подключены</numerusform>
+            <numerusform>%n клиентов подключено</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
---
+++ settings/translations/screencast.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>mainpage</name>
+    <message>
+        <location filename="../mainpage.qml" line="36"/>
+        <source>Screencast</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="40"/>
+        <source>Main options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="50"/>
+        <source>Buffers to store frames</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="51"/>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="64"/>
+        <source>JPEG frames quality</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="65"/>
+        <location filename="../mainpage.qml" line="79"/>
+        <source>%1%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="78"/>
+        <source>Frames scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="88"/>
+        <source>Apply smooth transformation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="98"/>
+        <source>Flush packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="107"/>
+        <source>Authorization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="113"/>
+        <location filename="../mainpage.qml" line="114"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="127"/>
+        <location filename="../mainpage.qml" line="128"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="138"/>
+        <source>You can connect to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="147"/>
+        <location filename="../mainpage.qml" line="148"/>
+        <source>
+http://%1%2:5554</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../mainpage.qml" line="152"/>
+        <source>%n connected clients</source>
+        <comment>0</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
