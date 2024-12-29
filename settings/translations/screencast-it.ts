518d58ff9dd5e9fadbf09ad5d65a2d59c51a2dcc
legacychimera247 <mad_fer_it@vivaldi.net>
2024-12-29 14:30:22 +0100

    Italian translation (#9)

---
+++ settings/translations/screencast-it.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>mainpage</name>
+    <message>
+        <location filename="../mainpage.qml" line="36"/>
+        <source>Screencast</source>
+        <translation>Condivisione schermo</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="40"/>
+        <source>Main options</source>
+        <translation>Impostazioni principali</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="50"/>
+        <source>Buffers to store frames</source>
+        <translation>Pre-caricamento dei frame</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="51"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="64"/>
+        <source>JPEG frames quality</source>
+        <translation>Qualità frame JPEG</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="65"/>
+        <location filename="../mainpage.qml" line="79"/>
+        <source>%1%</source>
+        <translation>%1%</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="78"/>
+        <source>Frames scale</source>
+        <translation>Dimensione frame</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="88"/>
+        <source>Apply smooth transformation</source>
+        <translation>Applica trasformazione uniforme</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="98"/>
+        <source>Flush packages</source>
+        <translation>Svuota pacchetti</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="107"/>
+        <source>Authorization</source>
+        <translation>Autorizzazione</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="113"/>
+        <location filename="../mainpage.qml" line="114"/>
+        <source>Username</source>
+        <translation>Nome utente</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="127"/>
+        <location filename="../mainpage.qml" line="128"/>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="138"/>
+        <source>You can connect to</source>
+        <translation>Puoi connetterti a</translation>
+    </message>
+    <message>
+        <location filename="../mainpage.qml" line="147"/>
+        <location filename="../mainpage.qml" line="148"/>
+        <source>
+http://%1%2:5554</source>
+        <translation>
+http://%1%2:5554</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../mainpage.qml" line="152"/>
+        <source>%n connected clients</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>%n client connessi</numerusform>
+            <numerusform>%n client connessi</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
